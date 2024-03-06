c211a83cb575f8997e80a9d86503dca3c58ab095
Josue Barrios Rodriguez <josue.barrios@gmail.com>
2024-03-06 11:11:07 -0500

    add cors policy

--- src/main/java/com/inclusion/technicaltest/infrastructure/RequiredReminderController.java
+++ src/main/java/com/inclusion/technicaltest/infrastructure/RequiredReminderController.java
@@ -6,6 +6,7 @@ import com.inclusion.technicaltest.model.RequestReminder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", allowCredentials = "false", exposedHeaders = {"Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"})
 public class RequiredReminderController {
 
    private final RequiredRemainder requiredRemainder;
